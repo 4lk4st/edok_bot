2f454e563b084e5a58458bbefc94025d8a82872d
Зайковский Всеволод <4lk4st@gmail.com>
2024-02-14 22:17:46 +0700

    add menu shifting after 09:45

--- keyboards/dao.py
+++ keyboards/dao.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from .data import menu
 
@@ -13,8 +13,25 @@ NUMBER_DAY = {
     6: "Воскресенье"
 }
 
-today_number = datetime.weekday(datetime.today())
-today_str = NUMBER_DAY[today_number] if today_number in range(5) else "Понедельник"
+def get_today_number() -> int:
+    today_number = datetime.weekday(datetime.today())
+
+    # блок для сравнения текущего времени со временем смены меню: 09:45 текущего дня
+    # переписать позже в отдельную функцию, с константами из .env файла
+    current_tomsk_time = datetime.utcnow() + timedelta(hours=7)
+    hours_minutes = current_tomsk_time.strftime("%H:%M")
+    hours, minutes = map(int, hours_minutes.split(':'))
+    current_time_one_int = hours + (minutes / 60)
+
+    time_x = 9 + (45 / 60)
+
+    if current_time_one_int > time_x:
+        today_number += 1
+
+    return today_number
+
+
+today_str = NUMBER_DAY[get_today_number()] if get_today_number() in range(5) else "Понедельник"
 
 def get_menu_sections() -> list[str]:
     menu_sections = list(menu[today_str].keys())
